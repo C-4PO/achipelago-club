6bce5a63e7708ec5f24330c3860db910261242c0
Christopher Bathgate <christopher.bathgate@gmail.com>
2023-06-25 10:15:28 -0400

    howler

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import { nodeLoaderPlugin } from "@vavite/node-loader/plugin"
 import { defineConfig } from 'vite';
 import inject from "@rollup/plugin-inject";
 import nodePolyfills from "rollup-plugin-node-polyfills";
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
 	plugins: [sveltekit(),nodeLoaderPlugin(),nodePolyfills()],
+	  // other config options
+	build: {
+		rollupOptions: {
+			plugins: [
+				inject({
+					Buffer: ['buffer', 'Buffer'],
+				}),
+			],
+		},
+	},
 });
