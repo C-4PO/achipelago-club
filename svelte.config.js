2155ed4507eae6c3507b020f71156c71fce9d3c7
Christopher Bathgate <christopher.bathgate@gmail.com>
2023-06-25 09:51:12 -0400

    add vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import svg from '@poppanator/sveltekit-svg';
+import adapter from '@sveltejs/adapter-vercel';
 
 const svgPlugin = svg()
 
