ac1f3936f07d812866a15d74202423928c215a0e
Christopher Bathgate <christopher.bathgate@gmail.com>
2023-06-25 10:06:45 -0400

    add buffer

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { nodeLoaderPlugin } from "@vavite/node-loader/plugin"
 import { defineConfig } from 'vite';
 import inject from "@rollup/plugin-inject";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
-	plugins: [sveltekit(),nodeLoaderPlugin()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        inject({
-          Buffer: ["buffer", "Buffer"],
-        }),
-      ],
-    },
-  },
+	plugins: [sveltekit(),nodeLoaderPlugin(),nodePolyfills()],
 });
