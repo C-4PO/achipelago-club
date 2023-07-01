d5cc633e019387c81137dc992ef4dc28fc0250f3
Christopher Bathgate <christopher.bathgate@gmail.com>
2023-07-01 12:31:39 -0400

    remove uneeded plugins

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 import { defineConfig } from 'vite';
-import polyfillNode from 'rollup-plugin-polyfill-node';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
     plugins: [
         sveltekit(),
-				commonjs(),
-        nodeLoaderPlugin(),
-        polyfillNode() // Add this line
     ],
 }); 
