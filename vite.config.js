b7dc5038919a6882ad3228e30247067456b8c20d
Christopher Bathgate <christopher.bathgate@gmail.com>
2023-07-01 11:25:46 -0400

    attempt to fix deployment again

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodeLoaderPlugin } from "@vavite/node-loader/plugin"
+import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 import { defineConfig } from 'vite';
-import nodePolyfills from "rollup-plugin-node-polyfills";
 import inject from '@rollup/plugin-inject';
+import { createRequire } from 'module'; // Bring in the ability to create the 'require' function
+
+const require = createRequire(import.meta.url); // construct the require function
 
 export default defineConfig({
-	plugins: [sveltekit(),nodeLoaderPlugin()],
-	  // other config options
+    plugins: [
+        sveltekit(),
+        nodeLoaderPlugin(),
+        inject({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ],
+    resolve: {
+        alias: {
+            buffer: require.resolve('buffer/'),
+        },
+    },
+    // other config options
 });
-
