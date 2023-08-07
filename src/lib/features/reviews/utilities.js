3c5ae7e87a6d6d9de5693d53bb2b25e3730b5135
Christopher Bathgate <christopher.bathgate@gmail.com>
2023-08-07 11:56:14 -0400

    restore 3 hour spacing

--- src/lib/features/reviews/utilities.js
+++ src/lib/features/reviews/utilities.js
@@ -3,7 +3,7 @@ import { supermemo } from 'supermemo';
 
 export function reviewCard({ review, grade, type }) {
   const MILLISECONDS_IN_3_HOURS = 3 * 60 * 60 * 1000;
-  const MILLISECONDS_IN_1_HOUR = 1 * 60 * 60 * 1000;
+  // const MILLISECONDS_IN_1_HOUR = 1 * 60 * 60 * 1000;
 
   if (!review) {
     review = {
@@ -20,7 +20,7 @@ export function reviewCard({ review, grade, type }) {
       interval,
       repetition,
       efactor,
-      due_date: dayjs().add(interval * MILLISECONDS_IN_1_HOUR).toDate(),
+      due_date: dayjs().add(interval *  MILLISECONDS_IN_3_HOURS).toDate(),
       last_reviewed: new Date(),
       type,
     }
